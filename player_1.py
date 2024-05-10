9e86a16aefb973bdcbd56a23247c9fc804df7081
maheeerrr <168569775+maheeerrr@users.noreply.github.com>
2024-05-10 08:44:04 -0400

    Add files via upload
    Added ball on the map; working on resizing it 
    Created timer for game of 2 minutes
    show the timer on screen; working on it to look better
    Created an end screen that states game is over when timer is over

---
+++ player_1.py
@@ -0,0 +1,26 @@
+import pygame
+
+
+class Player_1:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load("orange-fox-sprite.png")
+        self.image_size = self.image.get_size()
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+        self.delta = 4
+
+    def move_direction(self, direction):
+        if direction == "right":
+            self.x = self.x + self.delta
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+        if direction == "left":
+            self.x = self.x - self.delta
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+        if direction == "up":
+            self.y = self.y - self.delta
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+        if direction == "down":
+            self.y = self.y + self.delta
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
