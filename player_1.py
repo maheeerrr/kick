621387309fc1bb67d28dab5b33f896c0509390b5
maheeerrr <168569775+maheeerrr@users.noreply.github.com>
2024-05-28 08:43:05 -0400

    Update player_1.py

--- player_1.py
+++ player_1.py
@@ -18,10 +18,10 @@ class Player_1:
         if direction == "right" and self.x < 725 - 32:
             self.x = self.x + self.delta
         self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
-        if direction == "left" and self.x > 0 + 32:
+        if direction == "left" and self.x > 0 - 32:
             self.x = self.x - self.delta
         self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
-        if direction == "up" and self.y > 0 + 32:
+        if direction == "up" and self.y > 0 - 32:
             self.y = self.y - self.delta
         self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
         if direction == "down" and self.y < 363 - 32:
