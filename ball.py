bec2c8e3e89813e0f8f045653158267563dcc66f
maheeerrr <168569775+maheeerrr@users.noreply.github.com>
2024-05-30 08:45:26 -0400

    Update ball.py

--- ball.py
+++ ball.py
@@ -1,5 +1,7 @@
 import pygame
+import time
 
+BALL_ACTIVE_DURATION = 1
 
 class Ball:
 
@@ -12,8 +14,27 @@ class Ball:
         self.image = pygame.transform.scale(self.image, scale_size)
         self.image_size = self.image.get_size()
         self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+        self.velocity_x = 0
+        self.velocity_y = 0
+        self.start_time = None
+        self.radius = self.image_size[0] / 2
 
-    def set_location(self, new_x, new_y):
-        self.x = new_x
-        self.y = new_y
-        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+    def move(self):
+        self.x += self.velocity_x
+        self.y += self.velocity_y
+
+    def start_moving(self, direction):
+        if self.velocity_x == 0 and self.velocity_y == 0:
+            self.velocity_x, self.velocity_y = direction
+            self.start_time = time.time()
+
+    def update_velocity(self):
+        if self.start_time is not None:
+            elapsed_time = time.time() - self.start_time
+            if elapsed_time >= BALL_ACTIVE_DURATION:
+                self.velocity_x = 0
+                self.velocity_y = 0
+            else:
+                slow_down_factor = (BALL_ACTIVE_DURATION - elapsed_time) / BALL_ACTIVE_DURATION
+                self.velocity_x *= slow_down_factor
+                self.velocity_y *= slow_down_factor
