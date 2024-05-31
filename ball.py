ab5b5f74050f62a697ccd5cf529e2859231d9e41
maheeerrr <168569775+maheeerrr@users.noreply.github.com>
2024-05-31 08:41:39 -0400

    Update ball.py

--- ball.py
+++ ball.py
@@ -1,32 +1,30 @@
 import pygame
 import time
 
-BALL_ACTIVE_DURATION = 1
+BALL_ACTIVE_DURATION = .1
 
 class Ball:
-
-    def __init__(self, x, y):
+    def __init__(self, x, y, radius):
         self.x = x
         self.y = y
-        self.image = pygame.image.load("ball.png")
-        self.image_size = self.image.get_size()
-        scale_size = (self.image_size[0] * .05, self.image_size[1] * .05)
-        self.image = pygame.transform.scale(self.image, scale_size)
-        self.image_size = self.image.get_size()
-        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+        self.radius = radius
         self.velocity_x = 0
         self.velocity_y = 0
         self.start_time = None
-        self.radius = self.image_size[0] / 2
 
     def move(self):
         self.x += self.velocity_x
         self.y += self.velocity_y
 
+    def check_collision_with_walls(self, screen_width, screen_height):
+        if self.x - self.radius <= 0 or self.x + self.radius >= screen_width:
+            self.velocity_x = -self.velocity_x
+        if self.y - self.radius <= 0 or self.y + self.radius >= screen_height:
+            self.velocity_y = -self.velocity_y
+
     def start_moving(self, direction):
-        if self.velocity_x == 0 and self.velocity_y == 0:
-            self.velocity_x, self.velocity_y = direction
-            self.start_time = time.time()
+        self.velocity_x, self.velocity_y = direction
+        self.start_time = time.time()
 
     def update_velocity(self):
         if self.start_time is not None:
@@ -38,3 +36,6 @@ class Ball:
                 slow_down_factor = (BALL_ACTIVE_DURATION - elapsed_time) / BALL_ACTIVE_DURATION
                 self.velocity_x *= slow_down_factor
                 self.velocity_y *= slow_down_factor
+
+    def draw(self, surface):
+        pygame.draw.circle(surface, (255, 255, 255), (int(self.x), int(self.y)), self.radius)
